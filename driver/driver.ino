7e34b2f87511369cfa29478c78f3d50d5472d780
HenriPirinen <henripirinen@hotmail.fi>
2018-07-12 14:04:47 +0300

    Added param name to parameter request response

--- driver/driver.ino
+++ driver/driver.ino
@@ -1,11 +1,3 @@
-byte driveDirection = 0;
-byte setdriveDirection = 0;
-bool buttonState = true;
-bool lastButtonState = true; //Debounce
-byte reverse = 3; //Dig pin 3
-byte drive = 2; //Dig pin 2
-byte btnExec = 10; //Dig pin 10
-
 /**
    $getParams via Serial.print = request driver parameters from the server.
    0 = Neutral
@@ -14,25 +6,28 @@ byte btnExec = 10; //Dig pin 10
    99 = getDriverParams
 */
 
-void commands() {
+byte commands(byte currentDriveDir) {
   int command = 0;
+  byte driveDir = currentDriveDir;
+  
   if (Serial.available() > 0) {
     command = Serial.parseInt();
     switch (command) {
       case 0: {
-          driveDirection = 0;
+          driveDir = 0;
           break;
         }
       case 1: {
-          driveDirection = 1;
+          driveDir = 1;
           break;
         }
       case 2: {
-          driveDirection = 2;
+          driveDir = 2;
           break;
         }
       case 99: {
-          String _settings = "{\"type\":\"param\",\"direction\": " + String(driveDirection) + ",\"importance\":\"Low\"}";
+          //String _settings = "{\"type\":\"param\",\"direction\": " + String(driveDirection) + ",\"importance\":\"Low\"}";
+          String _settings = "{\"type\":\"param\",\"name\":\"direction\",\"value\": " + String(driveDir) + ",\"importance\":\"Low\"}";
           Serial.println(_settings);
           break;
         }
@@ -41,58 +36,69 @@ void commands() {
         }
     }
   }
+  return driveDir;
 }
 
-void setup() {
+int main(void) {
+  init();
+
+  byte driveDirection = 0;
+  byte setdriveDirection = 0;
+  bool buttonState = true;
+  bool lastButtonState = true; //Debounce
+  byte reverse = 3; //Dig pin 3
+  byte drive = 2; //Dig pin 2
+  byte btnExec = 10; //Dig pin 10
+  
   Serial.begin(9600);
   pinMode(13, OUTPUT);
   pinMode(drive, OUTPUT);
   pinMode(reverse, OUTPUT);
   pinMode(btnExec, INPUT);
-}
+  
+  while (true) {
+    driveDirection = commands(driveDirection); //Read serial on regular loops (requests / commands)
+    buttonState = digitalRead(btnExec);
+    if (buttonState != lastButtonState) {
 
-void loop() {
-  commands(); //Read serial on regular loops (requests / commands)
-  buttonState = digitalRead(btnExec);
-  if (buttonState != lastButtonState) {
+      if (buttonState == false) {
+        //Default state eg. button is not pressed.
+      } else {
+        Serial.println("$getParams");
+        Serial.println("{\"type\":\"log\",\"msg\":\"Get parameters from the server.\",\"importance\":\"Low\"}");
 
-    if (buttonState == false) {
-      //Default state eg. button is not pressed.
-    } else {
-      Serial.println("$getParams");
-      Serial.println("{\"type\":\"log\",\"msg\":\"Get parameters from the server.\",\"importance\":\"Low\"}");
+        do {
+          digitalWrite(13, HIGH); //Wait for response
+        } while (Serial.available() == 0);
+        digitalWrite(13, LOW);
+        driveDirection = commands(driveDirection); //Get response from the server
 
-      do {
-        digitalWrite(13, HIGH); //Wait for response
-      } while (Serial.available() == 0);
-      digitalWrite(13, LOW);
-      commands(); //Get response from the server
-
-      if (setdriveDirection != driveDirection) {
-        String _message = "{\"type\":\"log\",\"msg\":\"Set driver parameters: driveDirection: " + String(setdriveDirection) + " => " + String(driveDirection) + " \",\"importance\":\"Medium\"}";
-        setdriveDirection = driveDirection;
-        Serial.println(_message);
+        if (setdriveDirection != driveDirection) {
+          String _message = "{\"type\":\"log\",\"msg\":\"Set driver parameters: driveDirection: " + String(setdriveDirection) + " => " + String(driveDirection) + " \",\"importance\":\"Medium\"}";
+          setdriveDirection = driveDirection;
+          Serial.println(_message);
+        }
       }
+      delay(50);
     }
-    delay(50);
-  }
-  lastButtonState = buttonState;
+    lastButtonState = buttonState;
 
 
-  switch (setdriveDirection) {
-    case 0: //Neutral
-      digitalWrite(drive, HIGH);
-      digitalWrite(reverse, HIGH);
-      break;
-    case 1: //Reverse
-      digitalWrite(drive, HIGH);
-      digitalWrite(reverse, LOW);
-      break;
-    case 2: //Drive
-      digitalWrite(drive, LOW);
-      digitalWrite(reverse, HIGH);
-      break;
-    default:
-      Serial.println("{\"type\":\"log\",\"msg\":\"Invalid drive direction!\",\"importance\":\"High\"}");
+    switch (setdriveDirection) {
+      case 0: //Neutral
+        digitalWrite(drive, HIGH);
+        digitalWrite(reverse, HIGH);
+        break;
+      case 1: //Reverse
+        digitalWrite(drive, HIGH);
+        digitalWrite(reverse, LOW);
+        break;
+      case 2: //Drive
+        digitalWrite(drive, LOW);
+        digitalWrite(reverse, HIGH);
+        break;
+      default:
+        Serial.println("{\"type\":\"log\",\"msg\":\"Invalid drive direction!\",\"importance\":\"High\"}");
+    }
   }
 }
