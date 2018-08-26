a03b3e1f20c7f5ca74f626804690bc01cf3c299b
HenriPirinen <henripirinen@hotmail.fi>
2018-08-26 22:16:55 +0300

    Check if vehicle is charging

--- driver/driver.ino
+++ driver/driver.ino
@@ -1,18 +1,26 @@
+#define btnExec 10
+#define charge 11
+#define reverse 2
+#define forward 3
+#define cruise 4
+#define pump 5
+
 void commands() {
   String command = "0";
   if (Serial.available() > 0) {
     command = Serial.readString();
     if (command.charAt(0) == command.charAt(1)) {
-      digitalWrite(2, HIGH);
-      digitalWrite(3, HIGH);
+      digitalWrite(reverse, HIGH);
+      digitalWrite(forward, HIGH);
     } else {
-      digitalWrite(2, command.charAt(0) == '1' ? HIGH : LOW);
-      digitalWrite(3, command.charAt(1) == '1' ? HIGH : LOW);
+      digitalWrite(reverse, command.charAt(0) == '1' ? HIGH : LOW);
+      digitalWrite(forward, command.charAt(1) == '1' ? HIGH : LOW);
     }
-    digitalWrite(4, command.charAt(2) == '1' ? HIGH : LOW);
-    digitalWrite(5, command.charAt(3) == '1' ? HIGH : LOW);
+    digitalWrite(cruise, command.charAt(2) == '1' ? HIGH : LOW);
 
-    String _message = "{\"origin\":\"Driver\",\"type\":\"param\",\"value\":\"" + command + "\",\"importance\":\"Medium\"}";
+    String _message = "{\"origin\":\"Driver\",\"type\":\"param\",\"name\":\"driverState\",\"value\":\"" + command + "\",\"importance\":\"Medium\"}";
+    Serial.println(_message);
+    _message = "{\"origin\":\"Driver\",\"type\":\"log\",\"msg\":\"Set driver parameters: " + command + " \",\"importance\":\"Medium\"}";
     Serial.println(_message);
   }
 }
@@ -21,14 +29,15 @@ int main(void) {
   init();
   bool buttonState = true;
   bool lastButtonState = true; //Debounce
-  byte btnExec = 10; //Dig pin 10
+  bool isCharging = false;
 
   Serial.begin(9600);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
+  pinMode(reverse, OUTPUT);
+  pinMode(forward, OUTPUT);
+  pinMode(pump, OUTPUT);
   pinMode(btnExec, INPUT);
-  digitalWrite(2, HIGH);
-  digitalWrite(3, HIGH);
+  digitalWrite(reverse, HIGH);
+  digitalWrite(forward, HIGH);
 
   while (true) {
     buttonState = digitalRead(btnExec);
@@ -44,6 +53,14 @@ int main(void) {
         commands();
       }
       delay(50);
+    }
+    if(digitalRead(charge) == LOW){
+      digitalWrite(pump, LOW);
+      if(!isCharging) Serial.println("$charging");
+      isCharging = true;
+    } else {
+      if(isCharging) Serial.println("$!charging");
+      isCharging = false;
     }
     lastButtonState = buttonState;
   }
