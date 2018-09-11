ca5834e40034847635644cd0ce5069d7f273d45a
HenriPirinen <henripirinen@hotmail.fi>
2018-09-11 12:07:30 +0300

    11092018

--- driver/driver.ino
+++ driver/driver.ino
@@ -3,7 +3,7 @@
 #define reverse 2 //Write
 #define forward 3 //Write
 #define cruise 4 //Write
-#define pump 5 //Write
+#define webasto 5 //Write
 #define serialCharge 6 //Write
 
 void commands(bool charging) {
@@ -19,17 +19,21 @@ void commands(bool charging) {
         digitalWrite(forward, command.charAt(1) == '1' ? HIGH : LOW);
       }
       digitalWrite(cruise, command.charAt(2) == '1' ? HIGH : LOW);
+      delay(500);
+      digitalWrite(cruise, LOW);
 
       String _message = "{\"origin\":\"Driver\",\"type\":\"param\",\"name\":\"driverState\",\"value\":\"" + command + "\",\"importance\":\"Medium\"}";
       Serial.println(_message);
       _message = "{\"origin\":\"Driver\",\"type\":\"log\",\"msg\":\"Set driver parameters: " + command + " \",\"importance\":\"Medium\"}";
       Serial.println(_message);
     } else {
-      digitalWrite(serialCharge, command.charAt(2) == '0' ? HIGH : LOW); //SC0 = Stop serial charging, SC1 = Start serial charging (not used)
-      String _message = "{\"origin\":\"Driver\",\"type\":\"log\",\"msg\":\"Stopping serial charge\",\"importance\":\"Medium\"}";
-      Serial.println(_message);
-      _message = "$B1";
-      Serial.println(_message); //Send ACK to controller -> Controller can start balance state
+      if (command.charAt(0) == 'S' && command.charAt(1) == 'C') {
+        digitalWrite(serialCharge, command.charAt(2) == '0' ? HIGH : LOW); //SC0 = Stop serial charging, SC1 = Start serial charging (not used)
+        String _message = "{\"origin\":\"Driver\",\"type\":\"log\",\"msg\":\"Stopping serial charge\",\"importance\":\"Medium\"}";
+        Serial.println(_message);
+        _message = "$B1";
+        Serial.println(_message); //Send ACK to controller -> Controller can start balance state
+      }
     }
   }
 }
@@ -43,13 +47,14 @@ int main(void) {
   Serial.begin(9600);
   pinMode(reverse, OUTPUT);
   pinMode(forward, OUTPUT);
-  pinMode(pump, OUTPUT);
+  pinMode(webasto, OUTPUT);
   pinMode(serialCharge, OUTPUT);
   pinMode(btnExec, INPUT);
   pinMode(charge, INPUT);
   digitalWrite(reverse, HIGH);
   digitalWrite(forward, HIGH);
   digitalWrite(serialCharge, HIGH);
+  digitalWrite(webasto, HIGH);
 
   while (true) { //Main Loop
     buttonState = digitalRead(btnExec);
@@ -69,7 +74,6 @@ int main(void) {
       commands(true);
     }
     if (digitalRead(charge) == LOW) {
-      digitalWrite(pump, LOW);
       delay(1000);
       if (digitalRead(charge) == LOW) { //If charge is still LOW, socket is most likely in place.
         if (!isCharging) {
@@ -86,5 +90,17 @@ int main(void) {
       isCharging = false;
     }
     lastButtonState = buttonState;
+
+    if (Serial.available() > 0 && !isCharging) { //Instant action, does not require button push.
+      String _command = "";
+      _command = Serial.readString();
+      if (_command == "$webasto") {
+        digitalWrite(webasto, LOW);
+        Serial.println("{\"origin\":\"Driver\",\"type\":\"log\",\"msg\":\"Webasto ON.\",\"importance\":\"Low\"}");
+      } else if (_command == "$!webasto") {
+        digitalWrite(webasto, HIGH);
+        Serial.println("{\"origin\":\"Driver\",\"type\":\"log\",\"msg\":\"Webasto OFF.\",\"importance\":\"Low\"}");
+      }
+    }
   }
 }
